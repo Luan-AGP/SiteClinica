fe80edfc4fd8198afc8ad64b708c6ea1768b65ff
Luan Alexandre <103905179+Luan-AGP@users.noreply.github.com>
2023-01-24 21:58:57 -0300

    Add files via upload

---
+++ JS/INDEX.js
@@ -0,0 +1,57 @@
+const header = document.getElementById('header')
+const seta = document.getElementById('seta')
+const menuItems = document.querySelectorAll('.nav ul li a[href^="#"]');
+
+window.addEventListener('scroll', () => {
+    if ( window.scrollY > 950) {
+        header.style.setProperty('background-color', '#ffffff')
+        header.style.setProperty('box-shadow', '0 3px 10px -1px #A9A9A9')
+    } else {
+        header.style.setProperty('background-color', '')
+        header.style.setProperty('box-shadow', 'none')
+    }
+})
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 0;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
+ 
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 1000;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+};
